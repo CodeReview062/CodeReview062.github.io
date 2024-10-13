c2e0514cac1e7a645c5c94b11fe47ae84d75afdd
CodeReview062 <vent11235@gmail.com>
2024-10-14 05:04:10 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/src/main.tsx',
+    },
+  },
   plugins: [react()],
   base: "/",
   resolve: {
